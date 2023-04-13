1cbf57fb471be3c8925078354d0494db6422b88d
PucheDios <matiasp2004@gmail.com>
2023-04-13 14:13:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -86,35 +86,33 @@ while True:
     else:
         
         print(gestión_menú)
-        break
-
-sep()
-
-while True:
-    
-    sub_menú = input("¿Que parte del menú de 'Gestión de Locales' le gustaría ver?: ")
-    
-    if sub_menú == "b" or sub_menú == "c":
         
-        print("Lo lamentamos pero esta sección está en construcción")
-        
-    elif sub_menú == "a":
-        sep()
-        
-        print("Lamentamos informar se deben de almacenar 4(cuatro) locales en total si o si por ahora")
-        nombres = nombre1, nombre2, nombre3, nombre4 = input("Ingrese los nombres separados por espacios por favor: ").split()
-        rubros = rubro1, rubro2, rubro3, rubro4 = input("Ingrese los rubros separados por espacios acorde con el orden que los nombres: ").split()
-        ubicaciones = ubicación1, ubicación2, ubicación3, ubicación4 = input("Ingrese las ubicaciones separadas por espacios acorde con el orden que los rubros: ").split()
-        if rubro1 == rubro2 and rubro1 == rubro3  :
-            print(f"El rubro '{rubro1}' tiene la mayor cantidad de locales")
-        elif rubro2 == rubro3 and rubro2 == rubro4:
-            print(f"El rubro '{rubro2}' tiene la mayor cantidad de locales")
-        elif rubro3 == rubro4 and rubro3 == rubro1:
-            print(f"El rubro '{rubro3}' tiene la mayor cantidad de locales")
-        elif rubro4 == rubro1 and rubro4 == rubro2:
-            print(f"El rubro '{rubro4}' tiene la mayor cantidad de locales")
-        elif rubro4 == rubro2 and rubro4 == rubro3:
-            print(f"El rubro '{rubro2}' tiene la mayor cantidad de locales")
-        else: 
-            print("Ningún rubro tiene más locales que el otro")
-
+        while True:
+            
+            sub_menú = input("¿Que parte del menú de 'Gestión de Locales' le gustaría ver?: ")
+            sep()
+            if sub_menú == "b" or sub_menú == "c":
+                print("Lo lamentamos pero esta sección está en construcción")
+            
+            elif sub_menú == "a":
+                sep()
+                
+                print("Lamentamos informar se deben de almacenar 4(cuatro) locales en total si o si por ahora")
+                nombres = nombre1, nombre2, nombre3, nombre4 = input("Ingrese los nombres separados por espacios por favor: ").split()
+                rubros = rubro1, rubro2, rubro3, rubro4 = input("Ingrese los rubros separados por espacios acorde con el orden que los nombres: ").split()
+                ubicaciones = ubicación1, ubicación2, ubicación3, ubicación4 = input("Ingrese las ubicaciones separadas por espacios acorde con el orden que los rubros: ").split()
+                if rubro1 == rubro2 and rubro1 == rubro3  :
+                    print(f"El rubro '{rubro1}' tiene la mayor cantidad de locales")
+                elif rubro2 == rubro3 and rubro2 == rubro4:
+                    print(f"El rubro '{rubro2}' tiene la mayor cantidad de locales")
+                elif rubro3 == rubro4 and rubro3 == rubro1:
+                    print(f"El rubro '{rubro3}' tiene la mayor cantidad de locales")
+                elif rubro4 == rubro1 and rubro4 == rubro2:
+                    print(f"El rubro '{rubro4}' tiene la mayor cantidad de locales")
+                elif rubro4 == rubro2 and rubro4 == rubro3:
+                    print(f"El rubro '{rubro2}' tiene la mayor cantidad de locales")
+                else: 
+                    print("Ningún rubro tiene más locales que el otro")
+            else:
+                print("Volviendo...")
+                break
