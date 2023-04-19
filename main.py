8e0fdc3af27fcf0e79998e298c10f2aa99b77936
SantiKellem <93793504+SantiKellem@users.noreply.github.com>
2023-04-18 22:55:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ respuesta = inicio.lower()
 intentos = 0
 
 
-if respuesta == "si":
+if respuesta == "si" or respuesta == "sí":
     while intentos < 3:
         login_user = input("Ingrese su nombre de usuario: ")
         login_pass = getpass.getpass("Ingrese su contraseña: ")
@@ -73,19 +73,21 @@ perfumería = 0
 while True:
     elección = int(input("¿Que parte del menú principal le gustaría ver?: ")) 
     
-    if elección > 1:
+    if elección == 2 or elección == 3 or elección == 4 or elección == 5:
         print("Lo lamentamos pero esta sección está en construcción")
         
     elif elección == 0: 
-        salir()
+        salir()    
+    
+    elif elección != 1:
+        sep()
+        print("Opción inválida. Eliga una de las opciones disponibles.")
         
     else:
-        
         for clave, valor in gestión_menú.items():
             print(clave, ":", valor)
         
         while True:
-            
             sub_menú = input("¿Que parte del menú de 'Gestión de Locales' le gustaría ver?: ")
             sep()
             if sub_menú == "b" or sub_menú == "c":
@@ -93,7 +95,6 @@ while True:
             
             elif sub_menú == "a":
                 sep()
-                
                 print("Ingrese los datos de los locales que desee crear (máximo tres)")
                 nombres = nombre1, nombre2, nombre3 = input("Ingrese los nombres separados por comas, por favor: ").split(",")
                 rubros = rubro1, rubro2, rubro3 = input("Ingrese los rubros separados por espacios acorde al orden de su nombre: ").split()
@@ -115,7 +116,7 @@ while True:
                     print(f"El rubro con más locales es 'indumentaria', con {indumentaria} locales")
                 elif perfumería > indumentaria and perfumería > comida:
                     print(f"El rubro con más locales es 'perfumería', con {perfumería} locales")
-                
+                    
                 menor_local = min(comida, indumentaria, perfumería)
 
                 if menor_local == comida:
@@ -127,7 +128,7 @@ while True:
                     print(f"El rubro con menos locales es '{menor_local_nombre}' con {menor_local} locales")
             
             elif sub_menú != "d":
-                print("Opción inválida.")
+                print("Opción inválida. Eliga una de de las opciones disponibles.")
             
             else:
                 print("Volviendo...")
